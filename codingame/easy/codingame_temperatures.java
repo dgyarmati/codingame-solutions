3c642fb60949cc0fa2f2e9ec157b9bfe58bba079
dgyarmati <dgyarmati@protonmail.com>
2021-04-16 17:34:21 +0200

    cleanup

--- codingame/easy/codingame_temperatures.java
+++ codingame/easy/codingame_temperatures.java
@@ -3,6 +3,13 @@ import java.io.*;
 import java.math.*;
 import java.util.stream.*;
 
+/*
+https://www.codingame.com/ide/puzzle/temperatures
+
+Write a program that prints the temperature closest to 0 among input data. 
+If two numbers are equally close to zero, positive integer has to be considered closest to zero 
+(for instance, if the temperatures are -5 and 5, then display 5).
+ */
 class Solution {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
@@ -16,6 +23,8 @@ class Solution {
 
             for (int i = 0; i < N; i++) {
                 temperature = in.nextInt();
+				// the first condition simply checks for the lowest temperature. 
+				// the second runs on all values that are larger than the current min, filtering out cases when the two abs values are the same, choosing the positive. 
                 if (Math.abs(temperature) < Math.abs(min) || (temperature == -min && temperature > 0))
                     min = temperature;
             }
